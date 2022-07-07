3e3e9d2fd105779b1c0e90f3c305702e792f1561
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-07-08 01:02:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Pin from './Components/Pin';
 
 function App() {
+  const [Otp,setOtp]=useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pin length={4} OtpHandelar={(value)=>{setOtp(value)}}/>
+      <h4>OTP value of is {Otp}</h4>
     </div>
   );
 }
